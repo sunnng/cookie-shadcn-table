d732ce60726abd1cb3393af1aea2a8d1e42a0128
scy <sunchengyuan0706@hotmail.com>
2025-03-20 00:09:17 +0800

    guild battle page

--- src/app/(guilds)/guild/battle/page.tsx
+++ src/app/(guilds)/guild/battle/page.tsx
@@ -1,5 +1,14 @@
+import type { SearchParams } from '@/types'
 import { Shell } from '@/components/shell'
+import { getRecords } from './_lib/queries'
+import { searchParamsCache } from './_lib/validations'
 
-export default function GuildBattePage() {
-  return <Shell><div>公会讨伐战记录</div></Shell>
+interface IndexPageProps {
+  searchParams: Promise<SearchParams>
+}
+
+export default async function GuildBattePage({ searchParams }: IndexPageProps) {
+  const search = searchParamsCache.parse(await searchParams)
+  const { data, pageCount } = await getRecords(search)
+  return <Shell><div>{data.map(item => item.participantName)}</div></Shell>
 }
